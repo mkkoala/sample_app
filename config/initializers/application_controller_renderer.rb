197b3b3a224110eb7925a5fba722d55257744a40
mkkoala <mako1105.96@gmail.com>
2019-12-08 17:22:29 +0900

    [Finish] 3-10

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'fb9166ea6e66f4036988c55892c7efde6167c6753a2301c77bc906c40cf0262dec4d9505275436702064e847acac7723d8995ced25fc4457e6afc3e13c62617f'
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,9 @@ Rails.application.routes.draw do
 
   get 'todolists/:id' => 'todolists#show', as: 'todolist'
 
+  get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
+
+  patch 'todolist/:id' => 'todolists#update', as: 'update_todolist'
+
+  delete 'todolists/:id' => 'todolists#destroy', as: 'destroy_todolist'
 end
